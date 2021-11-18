7b0967e2d89c1809a237a6e5bd68801a1c1a6589
Alvaro Cardozo <alvaro_car6@hotmail.com>
2021-11-17 21:22:24 -0500

    Parcial

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 
-// Debug
-const gui = new dat.GUI()
+const HEIGHT_TABLE = 20
+const WITH_TABLE = 16
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -12,18 +11,28 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+// BoxGPU
+const boxGPU = new THREE.Mesh(new THREE.BoxGeometry(8, 1, 1),new THREE.MeshBasicMaterial({ color: 0x4CAF50 }))
+boxGPU.position.z = -HEIGHT_TABLE/2
+scene.add(boxGPU)
 
-// Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+// BoxPlayer
+const boxPlayer = new THREE.Mesh(new THREE.BoxGeometry(8, 1, 1),new THREE.MeshBasicMaterial({ color: 0xFFEB3B }))
+boxPlayer.position.z = HEIGHT_TABLE/2
+scene.add(boxPlayer)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
 
+// Ball
+const boxBall = new THREE.Mesh(new THREE.BoxGeometry(),new THREE.MeshBasicMaterial({ color: 0xE53935 }))
+scene.add(boxBall)
+
+
+// TABLE
+const planeTable = new THREE.Mesh(new THREE.PlaneGeometry(24, 21), new THREE.MeshBasicMaterial({color: 0x90A4AE}) )
+planeTable.rotation.x = 3*Math.PI/2
+planeTable.position.y = -.5
+scene.add(planeTable)
 // Lights
 
 const pointLight = new THREE.PointLight(0xffffff, 0.1)
@@ -60,14 +69,12 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
+camera.position.set(0, 15, 0)
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -78,22 +85,66 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// Define la direccion de la barra
+let rightBox = true
 
-const clock = new THREE.Clock()
+
+function boxGPUMove() {
+    if (rightBox) {
+        boxGPU.position.x += 0.05
+    } else {
+        boxGPU.position.x -= 0.05
+    }
+
+    if(boxGPU.position.x >= WITH_TABLE/2) {
+        rightBox = false
+    }
+    if(boxGPU.position.x <= -WITH_TABLE/2) {
+        rightBox = true
+    }
+}
+
+let ballDirection = true
+
+function ballMove() {
+    if (ballDirection) {
+        boxBall.position.z += 0.05
+    } else {
+        boxBall.position.z -= 0.05
+    }
+
+    if(boxBall.position.z >= HEIGHT_TABLE/2 - 1) {
+        ballDirection = false
+    }
+    if(boxBall.position.z <= -HEIGHT_TABLE/2 + 1) {
+        ballDirection = true
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    
+    if (e.key == "ArrowRight") {
+        if (boxPlayer.position.x >= WITH_TABLE/2) {
+            return
+        }
+        boxPlayer.position.x += 0.2
+        return
+    }
+    if (e.key == "ArrowLeft") {
+        if (boxPlayer.position.x <= -WITH_TABLE/2) {
+            return
+        }
+        boxPlayer.position.x -= 0.2
+        return
+    }
+})
 
 const tick = () =>
 {
 
-    const elapsedTime = clock.getElapsedTime()
+    boxGPUMove()
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
+    ballMove()
 
     // Render
     renderer.render(scene, camera)
